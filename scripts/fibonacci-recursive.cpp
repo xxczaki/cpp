6deb2a633b26c0e4a8058f63785d7d4aafd9c52d
Antoni Kepinski <xxczaki@pm.me>
2021-01-23 01:15:47 +0100

    Add files via upload

---
+++ scripts/fibonacci-recursive.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int fibonacci(int n) {
+    if (n <= 1)
+        return n;
+
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main() {
+    std::cout << fibonacci(9);
+}
